0c98a9cd367952127a60daffefb4972bd3c5fcf8
AnDee12 <ion-al@yandex.ru>
2019-03-22 14:28:59 +0300

    Create new Calculator

--- Calculator.java
+++ Calculator.java
@@ -1,18 +1,42 @@
 public class Calculator {
 
 	private int result;
+	private int first;
 
-	public void add(int ... paramo){
-		for (Integer param : paramo) {
+	public void add(int ... params){
+		for (Integer param : params) {
 			this.result += param;
 		}
 	}
 
+	public void subs(int ... params){
+		for (Integer param : params) {
+			this.result = first - param;
+			first = param;
+		}
+	}
+
+	public void pow(int ... params){
+		for (Integer param : params) {
+			this.result = first * param;
+			first = param;
+		}
+	}
+
+	public void divis(int ... params){
+		for (Integer param : params) {
+			this.result = first / param;
+			first = param;
+		}
+	}
+
 	public int getResult() {
+
 		return this.result;
 	}
 
 	public void cleanResult() {
+
 		this.result = 0;
 	}
 }
